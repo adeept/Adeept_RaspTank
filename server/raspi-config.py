dd62df363651f2f44c9b84fb85d6c9a5664cbcf5
Adeept <support@adeept.com>
2019-01-03 11:01:35 +0800

    Update raspi-config.py

--- server/raspi-config.py
+++ server/raspi-config.py
@@ -3,7 +3,7 @@
 # Description : The main program server takes control of Ultrasonic,Motor,Servo by receiving the order from the client through TCP and carrying out the corresponding operation.
 # Website     : www.adeept.com
 # E-mail      : support@adeept.com
-# Author      : William & Tony DiCola (tony@tonydicola.com, the WS_2812 code)
+# Author      : William
 # Date        : 2018/10/12
 
 import socket
