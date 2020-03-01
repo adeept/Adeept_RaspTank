4001a6908dff4e9462ac205ef513a5f4016eac4e
Mark Hetherington <mark@hethos.org>
2020-03-01 12:00:21 +1100

    Add installation of RPi.GPIO
    This was missing when settin up on my pi.

--- setup.py
+++ setup.py
@@ -94,6 +94,9 @@ for x in range(1,4):
 	if os.system("sudo pip3 install imutils zmq pybase64 psutil") == 0:   ####
 		break
 
+# Would be better to move to using requirements.txt for dependencies?
+os.system('sudo pip3 install RPi.GPIO')
+		
 for x in range(1,4):
 	if os.system("git clone https://github.com/oblique/create_ap") == 0:
 		break
@@ -126,6 +129,7 @@ try:
 		file_to_write.write("#!/bin/sh\n#sleep 10s\nsudo python3 //home/pi/adeept_rasptank/server/server.py")
 except:
 	pass
+
 
 os.system('sudo chmod 777 //home/pi/startup.sh')
 
